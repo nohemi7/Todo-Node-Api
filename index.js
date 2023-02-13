c7fad6f287477d26774c56c63fce6e4e6d74022b
Nohemi Morales <nohemi@umail.ucsb.edu>
2023-02-13 15:17:22 -0800

    Added POST endpoint

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const db = require('./database');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 4000;
+
+// Add body parser as middleware (processes requests 
+    // from user before endpoint)
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 // Endpoints
 
@@ -16,6 +22,37 @@ app.get('/api/todos', (req, res) => {
         }
         res.status(200).json(rows);
 
+    });
+});
+
+app.post('/api/todos', (req, res) => {
+    // Input Validation
+    var errors = [];
+    if(!req.body.name) {
+        errors.push("No Name supplied");
+    }
+    // If there are any validation errors
+    if(errors.length) {
+        res.status(400).json({ 'error': errors.join(',')});
+        return;
+    }
+
+    var data = {
+        name: req.body.name
+    }
+
+    var sql = 'INSERT INTO todos (name) VALUES (?);';
+    var params = [data.name];
+
+    db.run(sql, params, function(err, result) {
+        if(err) {
+            res.status(400).json({'error': err.message});
+        }
+
+        res.status(201).json({
+            id: this.lastID,
+            name: data.name
+        })
     });
 });
 
